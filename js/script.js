1c76c66b4aae85c2268db18dda4d448508021df0
Anatoly <oldcobbler@yandex.ru>
2022-06-20 22:40:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+console.log(5 + + '- object');
+
+console.log(4 + "6");
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(7%2);
+
+console.log(2*4 == 9);
+
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
